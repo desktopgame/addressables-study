c2213b8d10f203808ea1e3273b6bfc9855fa43c5
desktopgame <gnndr36@gmail.com>
2020-03-28 19:41:34 +0900

    ファイルの一覧を取得する処理実装

---
+++ Assets/Scripts/LoadAsset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UniRx;
+using UniRx.Async;
+
+public class LoadAsset : MonoBehaviour
+{
+    private List<string> assetIDList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var path = Path.Combine(Application.dataPath, "simple-assets");
+        this.assetIDList = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories).Select((e) => "Assets/" + e.Substring(Application.dataPath.Length + 1).Replace('\\', '/')).ToList();
+        assetIDList.ForEach(Debug.Log);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPush()
+    {
+        Load().Forget();
+    }
+
+    private async UniTask Load()
+    {
+    }
+}
